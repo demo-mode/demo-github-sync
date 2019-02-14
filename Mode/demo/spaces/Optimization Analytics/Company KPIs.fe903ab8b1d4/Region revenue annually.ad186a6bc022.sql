ed690beed5a07faeb0d7c084035340751dc6cad9
Sandra Liu <sandra@modeanalytics.com>
2019-02-14 22:53:23 +0000

    [Mode] ced

--- Mode/demo/spaces/Optimization Analytics/Company KPIs.fe903ab8b1d4/Region revenue annually.ad186a6bc022.sql
+++ Mode/demo/spaces/Optimization Analytics/Company KPIs.fe903ab8b1d4/Region revenue annually.ad186a6bc022.sql
@@ -5,7 +5,7 @@
            SUM(o.poster_qty) AS poster_units,
            SUM(o.standard_qty) AS standard_units,
            SUM(o.total_amt_usd) AS total_sales_usd,
-           (SUM(o.gloss_qty) + SUM(o.poster_qty)) / NULLIF(SUM(o.total),0)::float AS non_standard_sales
+           (SUM(o.gloss_qty) * SUM(o.poster_qty)) AS non_standard_sales
       FROM demo.accounts a
       JOIN demo.orders o
         ON o.account_id = a.id
@@ -19,4 +19,3 @@
         ON we.account_id = o.account_id
        AND o.occurred_at BETWEEN we.occurred_at AND we.occurred_at + interval '31 minutes'
      GROUP BY 1,2,3
-     --ORDER BY 1
