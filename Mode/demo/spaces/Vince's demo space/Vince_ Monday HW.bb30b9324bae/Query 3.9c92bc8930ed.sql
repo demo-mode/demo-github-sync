2fdfcbd0dbaad2dccf50ed6c824f76f2a1256a67
Vince Saldivar <vince@modeanalytics.com>
2019-09-23 22:18:12 +0000

    [Mode] Rename report 'Vince_ Monday HW'

--- Mode/demo/spaces/Vince's demo space/Vince_ Monday HW.bb30b9324bae/Query 3.9c92bc8930ed.sql
+++ Mode/demo/spaces/Vince's demo space/Vince_ Monday HW.bb30b9324bae/Query 3.9c92bc8930ed.sql
@@ -1,2 +1,10 @@
 SELECT*
 FROM tutorial.aapl_historical_stock_price
+{% form %}
+
+year:
+    type: multiselect
+    default: 2014
+    options: [2000,2001,2002,2003,2004,2005, 2006, 2007, 2008, 2009,2010,2011,2012,2013, 2014 ]
+
+{% endform %}
