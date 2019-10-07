3f38f0c0267454ab5de4a16460ec7623b40d6324
Sadavath Sharma <sadavath@modeanalytics.com>
2019-10-07 20:00:04 +0000

    [Mode] Create report 'Pre Helix - Parameters'

---
+++ Mode/demo/spaces/Helix Demo/Pre Helix - Parameters.4f940eb38b06/Parameters.10f474aa58c8.sql
@@ -0,0 +1,37 @@
+{% form %}
+
+Year:
+    type: multiselect
+    default: [1998, 1999, 2000, 2001, 2002, 2003]
+    options: [1998, 1999, 2000, 2001, 2002, 2003]
+
+Category:
+    type: multiselect
+    default: [Home, Men, Sports, Shoes, Jewelry, Women]
+    options: [Books, Children, Electronics, Home, Jewelry, 
+    Men, Music, Shoes, Sports, Women]
+    
+Class:
+    type: multiselect
+    default: [paint, accessories, fitness, mens, pendants, swimwear]
+    options: [accent, accessories, archery, arts, athletic, athletic shoes,
+    audio, automotive, baseball, basketball, bathroom, bedding, bridal,
+    blindes/shades, bracelets, business, camcorders, camers, camping, 
+    classical, computers, consignment, cooking, costume, country, curtains/drapes,
+    custom, decor, diamonds, disk drives, dresses, dvd/vcr players, earings, entertainments,
+    estate, fiction, fishing, fitness, flatware, football, fragrances, furniture,
+    glassware, gold, golf, guns, history, home repair, infants, jewelry boxes,
+    karoke, kids, lighting, loose stones, maternity, mattresses, memory, mens,
+    mens watch, monitors, musical, mystery, newborn, optics, outdoor, paint,
+    pants, parenting, pendants, personal, pools, pop, portable, reference, rings, rock,
+    romance, rugs, sailing, scanners, school-uniforms, science, self-help, semi-precious, 
+    shirts, sports, sports-apparel, stereo, swimwear, tables, televisions, tennis, toddlers,
+    travel, wallpaper, wireless, womens, womens watch]
+    
+State:
+    type: multiselect
+    default: [CA, FL, GA, OH, TX]
+    options: [AL, CA, CO, FL, GA, IA, IL, IN, KS, KY, LA, MD, MI, MN, MO, MT,
+    NC, NE, NJ, NM, NY, OH, OK, PA, SC, SD, TN, TX, VA, VT, WA, WI, WV]
+
+{% endform %}
---
+++ Mode/demo/spaces/Helix Demo/Pre Helix - Parameters.4f940eb38b06/Sales Dataset.63b8ea5ce5ea.sql
@@ -0,0 +1,55 @@
+SELECT  * 
+FROM  (SELECT d_year as year,
+              i_category as category, 
+              i_class as class, 
+              i_brand as brand, 
+                CASE
+                WHEN s_store_name = 'able' THEN 'Walmart'
+                WHEN s_store_name = 'anti' THEN 'Costco'
+                WHEN s_store_name = 'ation' THEN 'Target'
+                WHEN s_store_name = 'bar' THEN 'Safeway'
+                WHEN s_store_name = 'cally' THEN 'Trader Joes'
+                WHEN s_store_name = 'eing' THEN 'Whole Foods'
+                WHEN s_store_name = 'ese' THEN 'Ross'
+                WHEN s_store_name = 'n st' THEN 'Nordstrom'
+                WHEN s_store_name = 'ought' THEN 'Dicks'
+                WHEN s_store_name = 'pri' THEN 'Zara'
+                ELSE 'NoStore' END as store_name, 
+              s_state as state, 
+              d_moy as month, 
+              Sum(ss_sales_price) sum_sales,
+              Avg(Sum(ss_sales_price)) 
+                OVER ( 
+                  partition BY d_year, i_category, i_class, i_brand, s_store_name, s_state 
+                ) 
+                                  avg_monthly_sales 
+                                 
+       FROM roberto_salcido899.demoitemz i 
+       JOIN modeanalytics.demostoresalvesv5 ss 
+         ON ss_item_sk = i_item_sk 
+       JOIN roberto_salcido899.demodates d 
+         ON ss_sold_date_sk = d_date_sk 
+       JOIN roberto_salcido899.demostores s 
+         ON ss_store_sk = s_store_sk 
+      
+      WHERE i_category IN ( {{ Category }} )
+        AND i_class IN ( {{ Class }} )
+        AND s_state IN ( {{ State }} )
+        AND d_year IN ( {{ Year }} )
+        AND i_brand is NOT NULL
+        AND s_store_name is NOT NULL
+       
+      
+      GROUP BY d_year,
+                 i_category, 
+                 i_class, 
+                 i_brand, 
+                 s_store_name, 
+                 s_state, 
+                 d_moy) tmp1 
+                 
+                 
+
+ORDER BY sum_sales - avg_monthly_sales, 
+          tmp1.store_name
+          
