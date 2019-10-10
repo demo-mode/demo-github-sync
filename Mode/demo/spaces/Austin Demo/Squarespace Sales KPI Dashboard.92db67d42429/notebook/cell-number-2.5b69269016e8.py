5b4fc050075594634d1ce1382ac70bcaae99be59
Austin Bell <austin@modeanalytics.com>
2019-10-10 15:06:40 +0000

    [Mode] Create report 'Squarespace Sales KPI Dashboard'

---
+++ Mode/demo/spaces/Austin Demo/Squarespace Sales KPI Dashboard.92db67d42429/notebook/cell-number-2.5b69269016e8.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+daily = datasets["Forecasting"]
+
+day = pd.to_datetime(daily['day'])
+orders = daily['orders']
+
+plt.plot(day, orders)
