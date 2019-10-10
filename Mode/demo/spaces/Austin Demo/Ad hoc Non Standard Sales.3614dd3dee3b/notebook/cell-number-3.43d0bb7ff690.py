e5d3caa089b34572b536e72e019eba370eec9b83
Austin Bell <austin@modeanalytics.com>
2019-10-10 15:07:08 +0000

    [Mode] Create report 'Ad hoc Non Standard Sales'

---
+++ Mode/demo/spaces/Austin Demo/Ad hoc Non Standard Sales.3614dd3dee3b/notebook/cell-number-3.43d0bb7ff690.py
@@ -0,0 +1,16 @@
+# Grab SQL Results
+daily = datasets["Sales Data"]
+
+day = pd.to_datetime(daily['day'])
+sales = daily['sales']
+rolling = pd.rolling_mean(daily['sales'], 2, 0)
+
+idx = daily.index
+idx = np.array(idx, dtype=float)
+
+future = pd.Series(pd.date_range(day.min(),day.max() + pd.DateOffset(30)))
+future_idx = future.index
+future_idx = np.array(future_idx, dtype=float)
+
+z = np.polyfit(idx, rolling, 1)
+p = np.poly1d(z)
