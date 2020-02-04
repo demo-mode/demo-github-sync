a86a71a9423f4431504a3dc11537162125993448
Grant Miller <grant@modeanalytics.com>
2020-02-04 18:00:35 +0000

    [Mode] Rename Space 'MARKETING'

--- Mode/demo/spaces/MARKETING/Web Traffic - Paid Channels.d8c8112a0ff2/Query 1.f595fb549ed0.sql
+++
@@ -1,7 +0,0 @@
-    SELECT TO_CHAR(DATE_TRUNC('month',occurred_at),'YYYY-MM-DD')as month,
-           channel,
-           COUNT(*)
-      FROM demo.web_events
-     WHERE occurred_at < '2016-07-01'
-  GROUP BY 1,2
-  ORDER BY 1,3 DESC
--- Mode/demo/spaces/Marketing Dashboards/Sales by Marketing Channel (Definitions).f8e7dd6bbe22/Definition 2.88cdddbcccff.sql
+++ Mode/demo/spaces/Marketing Dashboards/Sales by Marketing Channel (Definitions).f8e7dd6bbe22/Definition 2.88cdddbcccff.sql
@@ -8,5 +8,4 @@
        AND o.occurred_at BETWEEN '2016-01-01' AND '2016-12-31 23:59:59'
   GROUP BY 1,2
   ORDER BY 1,2 DESC
-  
-  
+
