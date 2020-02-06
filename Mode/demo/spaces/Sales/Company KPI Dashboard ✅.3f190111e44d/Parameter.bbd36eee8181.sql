d23623b7f365542ff861885e2a8fb91245f3b290
Sadavath Sharma <sadavath@modeanalytics.com>
2020-02-06 03:08:42 +0000

    [Mode] Create report 'Company KPI Dashboard ✅'

---
+++ Mode/demo/spaces/Sales/Company KPI Dashboard ✅.3f190111e44d/Parameter.bbd36eee8181.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT(name) AS region
+FROM demo.region
+
+{% form %}
+
+region:
+  label: "Select a Region"
+  type: select
+  default: 'West'
+  options:
+    labels: region
+    values: region
+
+{% endform %}
