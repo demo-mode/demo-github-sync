7d942beaf18aafd6e42d6e58e92f7907d8b4059b
Austin Bell <austin@modeanalytics.com>
2020-04-06 15:19:49 +0000

    [Mode] Made changes queries 1 & 2

--- Mode/demo/spaces/Optimization Analytics/Company KPIs.fe903ab8b1d4/Orders.182a7113f111.sql
+++ Mode/demo/spaces/Optimization Analytics/Company KPIs.fe903ab8b1d4/Orders.182a7113f111.sql
@@ -1,6 +1,11 @@
+WITH
+
+  original_query as (
     SELECT day + INTERVAL '946 DAYS' AS day,
            orders,
-           rolling_avg
+           rolling_avg,
+           LAG(rolling_avg, 7) OVER(ORDER BY day + INTERVAL '946 DAYS') as rolling_avg_7d_ago,
+           COALESCE((rolling_avg - LAG(rolling_avg, 7) OVER(ORDER BY day + INTERVAL '946 DAYS')), 0) as rolling_avg_7d_ago_diff
       FROM (
     SELECT DATE_TRUNC('day',occurred_at) AS day,
            COUNT(*) AS orders,
@@ -9,4 +14,11 @@
      WHERE occurred_at < '2014-05-31'
      GROUP BY 1
            ) o
-     ORDER BY 1
+  )
+
+SELECT day,
+       orders,
+       rolling_avg,
+       rolling_avg_7d_ago_diff / rolling_avg_7d_ago as rolling_avg_7d_ago_percent_diff
+  FROM original_query
+ ORDER BY 1
