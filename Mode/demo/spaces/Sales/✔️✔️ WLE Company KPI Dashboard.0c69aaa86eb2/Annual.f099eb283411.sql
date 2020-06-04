57c7ad31680cbe3770e4786f5ee72caaeda5c216
Roberto Salcido <roberto@modeanalytics.com>
2020-06-04 23:58:34 +0000

    [Mode] Create report '✔️✔️ WLE Company KPI Dashboard'

---
+++ Mode/demo/spaces/Sales/✔️✔️ WLE Company KPI Dashboard.0c69aaa86eb2/Annual.f099eb283411.sql
@@ -0,0 +1,26 @@
+    SELECT DATE_TRUNC('month',o.occurred_at) + INTERVAL '1400 DAY' AS date,
+           a.name,
+           COUNT(DISTINCT o.account_id)+(random()*1000) AS customers,
+           SUM(o.gloss_qty) AS gloss_units,
+           SUM(o.poster_qty) AS poster_units,
+           SUM(o.standard_qty) AS standard_units,
+           SUM(o.total) AS total_units,
+           SUM(o.total_amt_usd) AS total_sales_usd,
+           (SUM(o.gloss_qty) + SUM(o.poster_qty)) / SUM(o.total)::float AS non_standard_sales
+      FROM demo.orders o
+      join demo.accounts a on a.id = o.account_id
+      join demo.sales_reps sr on sr.id = a.sales_rep_id 
+      join demo.region r on r.id = sr.region_id
+    Where a.id = {{ account_id }}
+
+      GROUP BY 1,2
+      ORDER BY 1,2
+      
+      
+{% form %}
+
+account_id:
+  type: text
+  default: 1001
+
+{% endform %}
