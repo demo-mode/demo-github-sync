4eed651ba4118ef370fa7c213760ff708a92ab7b
Jon Rosario <jon.rosario@modeanalytics.com>
2021-05-06 15:50:53 +0000

    [Mode] testing gh

--- Mode/demo/spaces/Sales/Monthly Sales KPI Dashboard.bfbe15af2171/01_ Units Sold.ba2e3148557e.sql
+++ Mode/demo/spaces/Sales/Monthly Sales KPI Dashboard.bfbe15af2171/01_ Units Sold.ba2e3148557e.sql
@@ -5,6 +5,6 @@ SELECT * FROM demo.web_events_full LIMIT 100;
     SELECT TO_CHAR(occurred_at,'YYYY')as year,
            SUM(total) as reams_sold
       FROM demo.orders
-     WHERE occurred_at BETWEEN '2015-01-01' AND '2016-12-31 23:59:59'
+     WHERE occurred_at BETWEEN '2015-01-02' AND '2016-12-31 23:59:59'
   GROUP BY 1
   ORDER BY 1 ASC
--- Mode/demo/spaces/Sales/Monthly Sales KPI Dashboard.bfbe15af2171/15_ 2016 Visits.5cfdbc940a4b.sql
+++ Mode/demo/spaces/Sales/Monthly Sales KPI Dashboard.bfbe15af2171/15_ 2016 Visits.5cfdbc940a4b.sql
@@ -1,7 +1,16 @@
-    SELECT TO_CHAR(occurred_at,'YYYY')as year,
-    TO_CHAR(occurred_at,'MM')as month,
-    COUNT(*)
-      FROM demo.web_events_full 
+WITH 
+
+  raw_data as (
+     SELECT TO_CHAR(occurred_at,'YYYY')as year,
+            TO_CHAR(occurred_at,'MM')as month,
+            COUNT(*)
+       FROM demo.web_events_full 
       where occurred_at <= '2017-01-01'
       group by 1,2
       order by 1,2
+  )
+
+SELECT ROW_NUMBER() OVER() as row_num,
+       *
+  FROM raw_data
+ ORDER BY 1
