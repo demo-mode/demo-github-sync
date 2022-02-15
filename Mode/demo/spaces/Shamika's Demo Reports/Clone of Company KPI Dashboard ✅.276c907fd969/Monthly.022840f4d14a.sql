5465808ddc2ee9630fc08ad055e02d2a04901d9e
Shamika Shah <shamika@modeanalytics.com>
2022-02-15 21:00:29 +0000

    [Mode] Create report 'Clone of Company KPI Dashboard ✅'

---
+++ Mode/demo/spaces/Shamika's Demo Reports/Clone of Company KPI Dashboard ✅.276c907fd969/Monthly.022840f4d14a.sql
@@ -0,0 +1,12 @@
+    SELECT DATE_TRUNC('day',o.occurred_at) AS Date,
+           --SUM(o.gloss_qty) AS "Glossy",
+           --SUM(o.poster_qty) AS "Poster",
+           --SUM(o.standard_qty) AS "Standard",
+           SUM(o.gloss_amt_usd) AS "Entertainment",
+           SUM(o.poster_amt_usd) AS "Sports",
+           SUM(o.standard_amt_usd) AS "News",
+           SUM(o.total_amt_usd) AS "Total Sales"
+      FROM demo.orders o
+     WHERE EXTRACT('year' from o.occurred_at) = 2016
+     GROUP BY 1
+     ORDER BY 1
---
+++ Mode/demo/spaces/Shamika's Demo Reports/Clone of Company KPI Dashboard ✅.276c907fd969/Parameters.bbc95e32251a.sql
@@ -0,0 +1,42 @@
+    SELECT TO_CHAR(DATE_TRUNC('{{ period }}',o.occurred_at),'YYYY-MM-DD')as period,
+           r.name as region,
+           sr.name as "sales rep",
+           a.name as customer,
+           SUM(o.total) as "reams sold",
+           SUM(o.total_amt_usd) as "revenue"
+      FROM demo.orders o
+      JOIN demo.accounts a
+        ON o.account_id = a.id
+      JOIN demo.sales_reps sr
+        ON a.sales_rep_id = sr.id
+      JOIN demo.region r
+        ON sr.region_id = r.id
+       AND CASE WHEN '{{ region }}' = 'All' THEN 1=1
+           ELSE '{{ region }}' = r.name END
+  GROUP BY 1,2,3,4
+  ORDER BY 1
+  
+{% form %}
+  
+period:
+  type: select
+  default: month
+  options: [day,week,month,quarter,year]
+  
+region:
+  type: select
+  default: Northeast
+  options: [[Northeast,Northeast],
+            [Midwest,Midwest],
+            [Southeast,Southeast],
+            [West,West],
+            [All,All]]
+            
+Customer:
+  type: multiselect
+  default: all
+  options:
+        labels: customer
+        values: customer
+
+{% endform %}
