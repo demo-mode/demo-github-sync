6d8ed3089713e47038015471807cd23592f7d330
Angel Catalan <angel@modeanalytics.com>
2022-02-25 14:13:24 +0000

    [Mode] Create report '⭐️ KPI Dashboard Example - AC'

---
+++ Mode/demo/spaces/Demo AC/⭐️ KPI Dashboard Example - AC.36d70ea8638e/Orders.b581a380e764.sql
@@ -0,0 +1,12 @@
+SELECT day + INTERVAL '1400 DAYS' AS day,
+           orders,
+           rolling_avg
+      FROM (
+    SELECT DATE_TRUNC('day',occurred_at) AS day,
+           COUNT(*) AS orders,
+           AVG(COUNT(*)) OVER (ORDER BY DATE_TRUNC('day',occurred_at) ROWS BETWEEN 6 PRECEDING AND 0 FOLLOWING) AS rolling_avg
+      FROM modeanalytics.dimension_orders 
+     WHERE occurred_at < '2014-05-31'
+     GROUP BY 1
+           ) o
+     ORDER BY 1
