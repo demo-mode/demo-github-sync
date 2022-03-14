e45eecc1211a9c8dda8b7721fd4c2f4d58b239e3
Grant Miller <grant@modeanalytics.com>
2022-03-14 16:51:41 +0000

    [Mode] Create report '⭐️ Rover KPI Dashboard Example'

---
+++ Mode/demo/spaces/Rover Analytics Demo/⭐️ Rover KPI Dashboard Example.f794872357bc/DailyRevenue.c2a199899164.sql
@@ -0,0 +1,24 @@
+SELECT
+  *,
+  CASE
+    WHEN time_id = CURRENT_DATE THEN 'Current'
+    WHEN time_id < CURRENT_DATE THEN 'Historic'
+    ELSE 'Future'
+  END AS time_category
+FROM
+  roberto_salcido899.solutionsdemodailyrevenue
+
+{% form %}
+
+segment:
+  label: "Select a Segment"
+  type: multiselect
+  default: all
+  options:
+    labels: segment
+    values: segment
+
+accountname:
+  type: text 
+
+{% endform %}
