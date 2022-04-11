4601b1bdd44ebc8a7995476f55747e29f12a3d7f
Kaitlin Hart <kaitlin.hart@modeanalytics.com>
2022-04-11 22:47:50 +0000

    [Mode] Create report 'Clone of Channel Sales Distribution Report'

---
+++ Mode/demo/spaces/Strategic Demo/Clone of Channel Sales Distribution Report.a6df2e4981ee/notebook/cell-number-2.d8285f14fc69.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib
+datasets
+df = datasets[0]
+df['created_at'] = df['created_at'].astype('datetime64[ns]')
+df['user_type'] = df['user_type'].astype('category')
+df.hist(column='session_duration_seconds', grid=False, figsize=(12,8))
