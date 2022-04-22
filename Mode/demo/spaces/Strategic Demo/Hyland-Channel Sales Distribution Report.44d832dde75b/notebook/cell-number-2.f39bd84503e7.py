76bd41f811abb2e588a15e817331b36a124f7ac0
Charlie Lopez <charlie@modeanalytics.com>
2022-04-22 12:47:53 +0000

    [Mode] Rename report 'Hyl-Channel Sales Distribution Report'

--- Mode/demo/spaces/Strategic Demo/Hyland-Channel Sales Distribution Report.44d832dde75b/notebook/cell-number-2.f39bd84503e7.py
+++ Mode/demo/spaces/Strategic Demo/Hyland-Channel Sales Distribution Report.44d832dde75b/notebook/cell-number-2.f39bd84503e7.py
@@ -4,5 +4,5 @@ datasets
 df = datasets[0]
 df['created_at'] = df['created_at'].astype('datetime64[ns]')
 df['user_type'] = df['user_type'].astype('category')
-df.hist(column='session_duration_seconds', grid=False, figsize=(12,8), color = "#6abf4b")
+df.hist(column='session_duration_seconds', grid=False, figsize=(11,6), color = "#6abf4b")
 ;
