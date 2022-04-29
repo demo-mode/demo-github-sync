82a4ced3c177c9ea8cb034f6f61cb36d38b411db
Charlie Lopez <charlie@modeanalytics.com>
2022-04-29 14:10:59 +0000

    [Mode] scale changing to a real estate dataset

--- Mode/demo/definitions/scale.sql
+++ Mode/demo/definitions/scale.sql
@@ -1,1 +1,32 @@
-SELECT 1
+SELECT *,
+       sub.calc_date_no_use + 2800 AS sale_date
+
+    FROM (
+SELECT home_type,
+       address,
+       city,
+       state,
+       zip,
+       list_price,
+       beds,
+       baths,
+       location,
+       sqft,
+       year_built,
+       parking_spots,
+       parking_type,
+       to_date(cast(last_sale_date as TEXT),'YYYY-MM-DD') AS calc_date_no_use,
+       last_sale_price AS sale_price,
+       url,
+       source,
+       listing_id,
+       CASE WHEN length(original_source) > 2
+       THEN left(original_source, 2)
+       ELSE original_source END AS agents,
+       latitude AS lat,
+       longitude AS lng,
+       is_short_sale,
+       floor(random()*(90-20+1))+20 AS days_on_market
+      FROM zhepu.sf_sold_homes 
+      
+) sub
