abcac679b89168e520a3d82b02b0e9c16b99b3b4
Reina Brenke <reina@mode.com>
2022-05-12 18:52:03 +0000

    [Mode] Create report '0b92574c7b58'

---
+++ Mode/demo/spaces/Ad Hoc - RB/0b92574c7b58/Query 1.fd7b4ba674da.sql
@@ -0,0 +1,1 @@
+SELECT * FROM tutorial.dunder_mifflin_paper_sales
---
+++ Mode/demo/spaces/Ad Hoc - RB/0b92574c7b58/Query 2.f8e67a26b258.sql
@@ -0,0 +1,12 @@
+SELECT * FROM {{ @Paper_Sales }}
+WHERE region in ({{ region }})
+
+
+{% form %}
+
+region:
+  type: multiselect
+  default: all 
+  options: [Northeast, Midwest, West, Southeast]
+  
+{% endform %}
