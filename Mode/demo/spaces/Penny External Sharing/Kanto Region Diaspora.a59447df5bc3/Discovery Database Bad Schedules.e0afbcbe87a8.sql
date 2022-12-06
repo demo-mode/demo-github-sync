d4fe65c73f26d406f15056631e2a799a27f089da
Penny Barnes <penny@mode.com>
2022-12-06 17:53:19 +0000

    [Mode] Rename report 'Query Analysis For Discovery Database'

--- Mode/demo/spaces/Penny External Sharing/Kanto Region Diaspora.a59447df5bc3/Discovery Database Bad Schedules.e0afbcbe87a8.sql
+++ Mode/demo/spaces/Penny External Sharing/Kanto Region Diaspora.a59447df5bc3/Discovery Database Bad Schedules.e0afbcbe87a8.sql
@@ -1,14 +1,3 @@
-/* ##################INSTRUCTIONS TO ENABLE QUERY TO RUN ON DISCOVERY DATABASE ##############################
-*
-*Because the Mode Public Warehouse runs on a postgreSQL database, we need to make a few changes to the syntax so the query can successfully run on the Discovery Database,
-*which is provided through Snowflake.
-*
-*Please make the following changes
-*1. Switch to your Discovery Database connection
-*2. Remove the comments around "mode."
-*3. Replace "NOW()" with CURRENT_TIMESTAMP
-
-*/
 
 WITH failed_scheduled_query_runs AS (
   SELECT
