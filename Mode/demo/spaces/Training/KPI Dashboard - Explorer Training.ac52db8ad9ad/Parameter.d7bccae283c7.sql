fbbe98ce37c737c190a961d384e2aae79e680235
Asia Davis <asia@mode.com>
2022-12-07 16:29:36 +0000

    [Mode] Create report 'KPI Dashboard - Explorer Training'

---
+++ Mode/demo/spaces/Training/KPI Dashboard - Explorer Training.ac52db8ad9ad/Parameter.d7bccae283c7.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT(name) AS region
+FROM demo.region
+
+{% form %}
+
+
+region:
+  label: "Select a Region"
+  type: multiselect
+  default: all
+  options:
+    labels: region
+    values: region
+
+{% endform %}
