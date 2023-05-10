520b98e2366fbd95f49f21a89c4d07e6564643b9
Axis Rutledge <axis.rutledge@mode.com>
2023-05-10 15:23:14 +0000

    [Mode] Create Report 'Clone of KPI Dashboard - exTaxReturn'

---
+++ Mode/demo/spaces/Axis Dashboards/Clone of KPI Dashboard - exTaxReturn.207ddbb79311/notebook/cell-number-3.43b185431412.py
@@ -0,0 +1,14 @@
+rolling = daily['orders'].rolling(7,0).mean()
+
+idx = daily.index
+idx = np.array(idx, dtype=float)
+
+future = pd.Series(pd.date_range(day.min(),day.max() + pd.DateOffset(90)))
+future_idx = future.index
+future_idx = np.array(future_idx, dtype=float )
+
+z = np.polyfit(idx, rolling, 1)
+p = np.poly1d(z)
+
+plt.plot(day, rolling)
+plt.plot(future, p(future_idx))
